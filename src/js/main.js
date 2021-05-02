921b16f801564c5c519dc22912f3280037cbdee1
Amantay <amantay.sv@gmail.com>
2021-05-02 14:26:32 +0600

    desktop

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+    items: 1,
+    loop: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    smartSpeed: 1000,
+  });
+  $(".owl-next").click(function () {
+    owl.trigger("next.owl.carousel");
+  });
+  $(".owl-prev").click(function () {
+    owl.trigger("prev.owl.carousel");
+  });
+});
