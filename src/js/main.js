5de7b4caab04e61a677a3cb324c737eeaf9098c8
Amantay <amantay.sv@gmail.com>
2021-05-03 04:54:39 +0600

    sticky

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
+$(window).on("scroll", function () {
+  AOS.init();
+});
+
 $(document).ready(function () {
   var owl = $(".owl-carousel");
   owl.owlCarousel({
@@ -17,11 +21,42 @@ $(document).ready(function () {
   });
 });
 
-let title = document.getElementById("header__title");
+window.onload = function () {
+  const parallax = document.querySelector(".promo");
+  const bg = document.querySelector(".promo__bg");
+  const title = document.getElementById("header__title");
+  const forBg = 10;
+  const speed = 0.05;
+  let positionX = 0;
+  let positionY = 0;
+  let coordXpercent = 0;
+  let coordYpercent = 0;
 
-window.addEventListener("scroll", function () {
-  let value = window.scrollY;
-  title.style.marginTop = value * 0.25 + "px";
-});
+  function setMouseParallaxStyle() {
+    const distX = coordXpercent - positionX;
+    const distY = coordYpercent - positionY;
+    positionX = positionX + distX * speed;
+    positionY = positionY + distY * speed;
+    bg.style.transform = `translate(${positionX / forBg}%, ${
+      positionY / forBg
+    }%)`;
+    requestAnimationFrame(setMouseParallaxStyle);
+  }
+  setMouseParallaxStyle();
 
-AOS.init();
+  parallax.addEventListener("mousemove", function (e) {
+    const parallaxWidth = parallax.offsetWidth;
+    const parallaxHeight = parallax.offsetHeight;
+    const coordX = e.pageX - parallaxWidth / 2;
+    const coordY = e.pageY - parallaxHeight / 2;
+    coordXpercent = (coordX / parallaxWidth) * 100;
+    coordYpercent = (coordY / parallaxHeight) * 100;
+  });
+
+
+  window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    title.style.marginTop = value * 0.35 + "px";
+    bg.style.marginTop = value * 0.35 + "px";
+  });
+};
